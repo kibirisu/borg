a9cfcb60e8064ec206a6e30c6ef0dcbe8d57c015
kibirisu <szymon@wabisabi.cafe>
2025-11-20 16:38:48 +0100

    web: integrate data fetching with react router

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,24 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useLoaderData,
+} from "react-router";
 import App from "./App";
 import MainFeed from "./components/feed/MainFeed";
 import UserProfile from "./components/profile/UserProfile";
 import { sampleUsers, type User } from "./components/feed/feedData";
 import type { LoaderFunctionArgs } from "react-router";
 import { newClient, type Client } from "./lib/api/client";
+import { QueryClientProvider, useSuspenseQuery } from "@tanstack/react-query";
 
 const client = newClient();
+
+const Component = () => {
+  const { opts } = useLoaderData() as Awaited<
+    ReturnType<ReturnType<typeof loader>>
+  >;
+  const { data } = useSuspenseQuery(opts);
+  return <>{data.username}</>;
+};
 
 const loader =
   (client: Client) =>
     async ({ params }: LoaderFunctionArgs) => {
-      const foo = client.$api.queryOptions("get", "/api/users/{id}", {
-        params: { path: { id: 1 } },
+      const userId = parseInt(String(params.handle));
+      const opts = client.$api.queryOptions("get", "/api/users/{id}", {
+        params: { path: { id: userId } },
       });
-      console.log(foo);
-      console.log(params);
-      return foo;
+      await client.queryClient.ensureQueryData(opts);
+      return { userId: userId, opts: opts };
     };
 
 const router = createBrowserRouter([
@@ -36,8 +49,8 @@ const router = createBrowserRouter([
         },
       },
       {
-        path: "foo",
-        element: <span>bar</span>,
+        path: "foo/:handle",
+        Component: Component,
         loader: loader(client),
       },
       // {
@@ -56,7 +69,9 @@ if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={client.queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </React.StrictMode>,
   );
 }
