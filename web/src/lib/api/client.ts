55dbcd2f9ffc94106b1417febb9dc29563f31357
kibirisu <szymon@wabisabi.cafe>
2025-11-24 15:28:31 +0100

    web: add auth middleware

--- web/src/lib/api/client.ts
+++ web/src/lib/api/client.ts
@@ -1,6 +1,6 @@
 import type { paths } from "./v1";
 import { QueryClient } from "@tanstack/react-query";
-import createFetchClient from "openapi-fetch";
+import createFetchClient, { type Middleware } from "openapi-fetch";
 import createClient, { type OpenapiQueryClient } from "openapi-react-query";
 
 export interface Client {
@@ -10,9 +10,22 @@ export interface Client {
 
 export default function newClient(): Client {
   const fetchClient = createFetchClient<paths>();
+  fetchClient.use(authMiddleware);
   const $api = createClient(fetchClient);
   const queryClient = new QueryClient({
     defaultOptions: { queries: { staleTime: 1000 * 10 } },
   });
+  accessToken = localStorage.getItem("jwt");
   return { queryClient, $api };
 }
+
+let accessToken: string | null = null;
+
+const authMiddleware: Middleware = {
+  async onRequest({ request }) {
+    if (accessToken) {
+      request.headers.set("Authorization", `Bearer ${accessToken}`);
+    }
+    return request;
+  },
+};
