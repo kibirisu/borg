f8ecc98036af1dc36e297ab9a098c67b44ada8b0
kibirisu <szymon@wabisabi.cafe>
2025-12-01 22:32:37 +0100

    web: add login modal form

--- web/src/components/bar/AuthButtons.tsx
+++ web/src/components/bar/AuthButtons.tsx
@@ -1,8 +1,40 @@
+import { useRef } from "react";
+
 const AuthButtons = () => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
   return (
     <>
       <a className="btn">Sign Up</a>
-      <a className="btn">Sign In</a>
+      <button
+        className="btn"
+        onClick={() => {
+          if (dialogRef.current) {
+            dialogRef.current.showModal();
+          }
+        }}
+      >
+        Sign In
+      </button>
+      <dialog ref={dialogRef} className="modal">
+        <div className="modal-box max-w-min">
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+            <legend className="fieldset-legend">Login</legend>
+
+            <label className="label">Email</label>
+            <input type="email" className="input" placeholder="Email" />
+
+            <label className="label">Password</label>
+            <input type="password" className="input" placeholder="Password" />
+
+            <button className="btn btn-neutral mt-4">Login</button>
+          </fieldset>
+        </div>
+
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
     </>
   );
 };
