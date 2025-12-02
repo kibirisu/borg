afe543535a7f96b278c8fc9b33a91e80232d9e7c
Jan Tacakiewicz <jan.tacakiewicz@protonmail.com>
2025-12-02 21:33:19 +0100

    web: feed: MainFeed: added post form to main feed

--- web/src/components/feed/MainFeed.tsx
+++ web/src/components/feed/MainFeed.tsx
@@ -1,4 +1,6 @@
+import { Outlet } from "react-router";
 import type { Client } from "../../lib/client";
+import NewPostForm from "./NewPostForm";
 
 export const loader = (client: Client) => async () => {
   try {
@@ -6,9 +8,18 @@ export const loader = (client: Client) => async () => {
     if (!opts) {
       throw new Error("queryOptions returned undefined");
     }
+    client.queryClient.prefetchQuery({ ...opts, staleTime: 0 });
     return { opts };
   } catch (error) {
     console.error("Loader error:", error);
     return { opts: undefined as any };
   }
 };
+export default function MainFeed() {
+  return (
+    <div className="max-w-2xl mx-auto border-x border-gray-300 min-h-screen bg-white">
+      <NewPostForm />
+      <Outlet />
+    </div>
+  );
+}
