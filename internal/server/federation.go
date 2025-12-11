dda52786c5d66ecff38226f3278c1ef766799472
kibirisu <szymon@wabisabi.cafe>
2025-12-11 21:50:14 +0100

    AP: sample webfinger

--- internal/server/federation.go
+++ internal/server/federation.go
@@ -3,6 +3,7 @@ package server
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/kibirisu/borg/internal/api"
 )
@@ -17,7 +18,23 @@ func (s *Server) GetWellKnownWebfinger(
 	// We should provide the identifier here
 	// In ActivityStreams all objects "should have unique global identifiers"
 	var resp api.WebFingerResponse
-	resp.Subject = params.Resource
+	resource := strings.TrimPrefix(params.Resource, "acct:")
+
+	// Here we should query database for a user contained in resource
+	// And return minimal response as defined in WebFinger spec
+
+	// WebFinger pointing to queried actor
+	resp.Subject = resource
+	resp.Links = append(
+		resp.Links,
+		api.WebFingerLink{
+			Rel:  "self",
+			Type: "application/activity+json",
+			Href: "http://127.0.0.1:8080/users/user", // ActivityPub user URI
+		},
+	)
+
 	json.NewEncoder(w).Encode(&resp)
+	w.WriteHeader(http.StatusOK)
 }
 
