b5bd6798ad82514b6605725b5f5b052aafe71d63
kibirisu <szymon@wabisabi.cafe>
2025-12-15 23:19:43 +0100

    Fix codegen, add auth middleware for investigation

--- cmd/dev/main.go
+++ cmd/dev/main.go
@@ -1,22 +1,43 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
+	"github.com/getkin/kin-openapi/openapi3filter"
 	"github.com/go-chi/chi/v5"
+	middleware "github.com/oapi-codegen/nethttp-middleware"
+
+	staging "github.com/kibirisu/borg/pkg"
 )
 
 type server struct{}
 
-var s server
+var srv server
+
+var _ staging.ServerInterface = (*server)(nil)
 
 // GetFoo implements ServerInterface.
 func (s *server) GetFoo(w http.ResponseWriter, r *http.Request) {
-	panic("unimplemented")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("bar"))
+}
+
+func createMiddleware() func(http.Handler) http.Handler {
+	spec, _ := staging.GetSwagger()
+	spec.Servers = nil
+	return middleware.OapiRequestValidatorWithOptions(spec, &middleware.Options{
+		Options: openapi3filter.Options{
+			AuthenticationFunc: func(ctx context.Context, ai *openapi3filter.AuthenticationInput) error {
+				return nil
+			},
+		},
+	})
 }
 
 func main() {
 	r := chi.NewMux()
-	r.Get("/", s.GetFoo)
+	r.Use(createMiddleware())
+	r.Get("/foo", srv.GetFoo)
 	panic(http.ListenAndServe(":8080", r))
 }
