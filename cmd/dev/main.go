485eb19a907c0d52ba091f0afde3cf6b762e7e22
kibirisu <szymon@wabisabi.cafe>
2025-12-16 15:59:19 +0100

    Revert "Fix codegen, add auth middleware for investigation"
    This reverts commit b5bd6798ad82514b6605725b5f5b052aafe71d63.

--- cmd/dev/main.go
+++ cmd/dev/main.go
@@ -1,43 +1,22 @@
 package main
 
 import (
-	"context"
 	"net/http"
 
-	"github.com/getkin/kin-openapi/openapi3filter"
 	"github.com/go-chi/chi/v5"
-	middleware "github.com/oapi-codegen/nethttp-middleware"
-
-	staging "github.com/kibirisu/borg/pkg"
 )
 
 type server struct{}
 
-var srv server
-
-var _ staging.ServerInterface = (*server)(nil)
+var s server
 
 // GetFoo implements ServerInterface.
 func (s *server) GetFoo(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte("bar"))
-}
-
-func createMiddleware() func(http.Handler) http.Handler {
-	spec, _ := staging.GetSwagger()
-	spec.Servers = nil
-	return middleware.OapiRequestValidatorWithOptions(spec, &middleware.Options{
-		Options: openapi3filter.Options{
-			AuthenticationFunc: func(ctx context.Context, ai *openapi3filter.AuthenticationInput) error {
-				return nil
-			},
-		},
-	})
+	panic("unimplemented")
 }
 
 func main() {
 	r := chi.NewMux()
-	r.Use(createMiddleware())
-	r.Get("/foo", srv.GetFoo)
+	r.Get("/", s.GetFoo)
 	panic(http.ListenAndServe(":8080", r))
 }
