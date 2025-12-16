3577f3c3dcdff7df3e436c4026370556d13c5833
kibirisu <szymon@wabisabi.cafe>
2025-12-16 12:24:30 +0100

    Prove container providing middleware works

--- cmd/dev/main.go
+++ cmd/dev/main.go
@@ -13,14 +13,32 @@ import (
 
 type server struct{}
 
+type product struct {
+	name *string
+}
+
+type container struct {
+	item *product
+}
+
 var srv server
 
 var _ staging.ServerInterface = (*server)(nil)
 
 // GetFoo implements ServerInterface.
 func (s *server) GetFoo(w http.ResponseWriter, r *http.Request) {
+	cont := r.Context().Value("container").(*container)
+	println(*cont.item.name)
 	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte("bar"))
+	_, _ = w.Write([]byte("bar"))
+}
+
+func preAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cont := &container{nil}
+		ctx := context.WithValue(r.Context(), "container", cont)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
 
 func createMiddleware() func(http.Handler) http.Handler {
@@ -29,6 +47,9 @@ func createMiddleware() func(http.Handler) http.Handler {
 	return middleware.OapiRequestValidatorWithOptions(spec, &middleware.Options{
 		Options: openapi3filter.Options{
 			AuthenticationFunc: func(ctx context.Context, ai *openapi3filter.AuthenticationInput) error {
+				cont := ctx.Value("container").(*container)
+				name := "archlinux"
+				cont.item = &product{&name}
 				return nil
 			},
 		},
@@ -37,7 +58,7 @@ func createMiddleware() func(http.Handler) http.Handler {
 
 func main() {
 	r := chi.NewMux()
-	r.Use(createMiddleware())
+	r.Use(preAuthMiddleware, createMiddleware())
 	r.Get("/foo", srv.GetFoo)
 	panic(http.ListenAndServe(":8080", r))
 }
