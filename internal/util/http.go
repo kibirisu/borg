9e07a90f5500a5b4152c650a0e34c8d4262401b8
Jan Tacakiewicz <jan.tacakiewicz@protonmail.com>
2025-12-25 21:29:28 +0100

    internal: util: http: added http utils that are useful for AP sync messages propagation

--- internal/util/http.go
+++ internal/util/http.go
@@ -1,8 +1,13 @@
 package util
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
 	"net/http"
+	"time"
+
+	"github.com/kibirisu/borg/internal/service"
 )
 
 func WriteJSON(w http.ResponseWriter, status int, data any) {
@@ -35,3 +40,44 @@ func WriteError(w http.ResponseWriter, status int, message string) {
 func ReadJSON(r *http.Request, dst any) error {
 	return json.NewDecoder(r.Body).Decode(dst)
 }
+
+func DeliverToEndpoint(endpoint string, payload any) {
+	if endpoint == "" {
+		return
+	}
+	go func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		jsonData, err := json.Marshal(payload)
+		if err != nil {
+			return
+		}
+		req, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint, bytes.NewBuffer(jsonData))
+		if err != nil {
+			return
+		}
+		req.Header.Set("Content-Type", "application/json")
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			return
+		}
+		defer resp.Body.Close()
+	}()
+}
+
+func DeliverToFollowers(app service.AppService,
+	w http.ResponseWriter, r *http.Request, userID int,
+	build func(recipientURI string) any,
+) {
+	followers, err := app.GetAccountFollowers(r.Context(), userID);
+    if err != nil {
+        http.Error(w, "Internal server error", http.StatusInternalServerError)
+        return
+    }
+	for _, follower := range followers {
+		payload := build(follower.Uri)
+		DeliverToEndpoint(follower.InboxUri, payload)
+	}
+}
+
