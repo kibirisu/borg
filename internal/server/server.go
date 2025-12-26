104201ef3bdd4eb55ccfd1268015de3dc8880e36
kibirisu <szymon@wabisabi.cafe>
2025-12-26 18:50:01 +0100

    move preAuthMiddleware to correct group

--- internal/server/server.go
+++ internal/server/server.go
@@ -34,7 +34,6 @@ func New(conf *config.Config) *http.Server {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	r.Use(preAuthMiddleware)
 	r.Group(server.federationRoutes())
 	r.Group(server.staticRoutes())
 
