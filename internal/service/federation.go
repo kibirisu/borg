a2e8931f366579bd3cf7f0a74fcfd75dad08042b
phootoon <115477523+phootoon@users.noreply.github.com>
2026-01-04 16:43:40 +0100

    added processing incoming activity to get its type
    Interfejs FederationService: Dodałem metodę ProcessIncomingActivity(context.Context, *domain.Object) error.
    Dispatcher (switch raw.Type): Zaimplementowałem ProcessIncomingActivity, który rozpoznaje typ aktywności.
    Wrapper NewCreateActivity: Wykorzystałem istniejący w internal/ap wrapper, aby bezpiecznie rzutować surowy domain.Object na typowaną strukturę ap.Activity[ap.Note].
    handleCreate: Dodałem metodę obsługującą Create, która na razie (zgodnie z prośbą) tylko wyciąga dane (actorID, noteContent) i loguje je, przygotowując miejsce pod przyszłe "hitnięcie endpointu" w celu pobrania aktora.
    Czyszczenie pliku: Usunąłem fragment PostInbox z końca pliku, ponieważ był to kod handlera HTTP (*Server), który nie powinien znajdować się w warstwie serwisu (federationService), a powodowałby błędy kompilacji.

--- internal/service/federation.go
+++ internal/service/federation.go
@@ -2,7 +2,10 @@ package service
 
 import (
 	"context"
+	"fmt"
+	"log"
 
+	"github.com/kibirisu/borg/internal/ap"
 	"github.com/kibirisu/borg/internal/db"
 	"github.com/kibirisu/borg/internal/domain"
 	repo "github.com/kibirisu/borg/internal/repository"
@@ -11,6 +14,7 @@ import (
 type FederationService interface {
 	GetLocalActor(context.Context, string) (*domain.Actor, error)
 	CreateActor(context.Context, db.CreateActorParams) (*db.Account, error)
+	ProcessIncomingActivity(context.Context, *domain.Object) error
 }
 
 type federationService struct {
@@ -25,27 +29,27 @@ var _ FederationService = (*federationService)(nil)
 
 // GetLocalActor implements FederationService.
 // not using anymore
-func (s *federationService) GetLocalActor(
-	ctx context.Context,
-	username string,
-) (*domain.Actor, error) {
-	account, err := s.store.Accounts().GetLocalByUsername(ctx, username)
-	if err != nil {
-		return nil, err
-	}
-	// we probably would implement mapper functions
-	actor := domain.Actor{
-		Context:           "https://www.w3.org/ns/activitystreams",
-		ID:                account.Uri,
-		Type:              "Person",
-		PreferredUsername: account.Username,
-		Inbox:             account.InboxUri,
-		Outbox:            account.OutboxUri,
-		Following:         account.FollowingUri,
-		Followers:         account.FollowersUri,
-	}
-	return &actor, nil
-}
+// func (s *federationService) GetLocalActor(
+// 	ctx context.Context,
+// 	username string,
+// ) (*domain.Actor, error) {
+// 	account, err := s.store.Accounts().GetLocalByUsername(ctx, username)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	// we probably would implement mapper functions
+// 	actor := domain.Actor{
+// 		Context:           "https://www.w3.org/ns/activitystreams",
+// 		ID:                account.Uri,
+// 		Type:              "Person",
+// 		PreferredUsername: account.Username,
+// 		Inbox:             account.InboxUri,
+// 		Outbox:            account.OutboxUri,
+// 		Following:         account.FollowingUri,
+// 		Followers:         account.FollowersUri,
+// 	}
+// 	return &actor, nil
+// }
 
 // CreateActor implements FederationService.
 func (s *federationService) CreateActor(
@@ -54,4 +58,37 @@ func (s *federationService) CreateActor(
 ) (*db.Account, error) {
 	account, err := s.store.Accounts().Create(ctx, actor)
 	return &account, err
+}
+
+// ProcessIncomingActivity implements FederationService.
+func (s *federationService) ProcessIncomingActivity(ctx context.Context, raw *domain.Object) error {
+	log.Printf("Processing incoming activity: %s (ID: %s)", raw.Type, raw.ID)
+
+	switch raw.Type {
+	case "Create":
+		wrappedRaw := &domain.ObjectOrLink{Object: raw}
+		activityWrapper := ap.NewCreateActivity(wrappedRaw)
+		return s.handleCreate(ctx, activityWrapper.GetObject())
+
+	case "Follow":
+		return fmt.Errorf("Follow activity not implemented yet")
+
+	case "Undo":
+		return fmt.Errorf("Undo activity not implemented yet")
+
+	default:
+		return fmt.Errorf("unsupported activity type: %s", raw.Type)
+	}
+}
+
+func (s *federationService) handleCreate(ctx context.Context, activity ap.Activity[ap.Note]) error {
+	// TODO: Fetch remote actor if not exists (hit endpoint)
+	actorID := activity.Actor.GetRaw().Object.ID
+
+	// Extract Note content
+	noteContent := activity.Object.GetObject().Content
+
+	log.Printf("Authorized Actor %s created a Note: %s", actorID, noteContent)
+
+	return nil
 }
