bb4a259d7d60d32b338301f088c6efaba0e32e36
Jan Tacakiewicz <jan.tacakiewicz@protonmail.com>
2026-01-10 17:44:01 +0100

    internal: ap: added missing like activity definition

---
+++ internal/ap/like.go
@@ -0,0 +1,41 @@
+package ap
+
+import "github.com/kibirisu/borg/internal/domain"
+
+type LikeActivitier interface {
+	Activiter[Note]
+}
+
+type likeActivity struct {
+	activity
+}
+
+var _ LikeActivitier = (*likeActivity)(nil)
+
+func NewLikeActivity(from *domain.ObjectOrLink) LikeActivitier {
+	return &likeActivity{activity{object{from}}}
+}
+
+// GetObject implements likeActivitier.
+// Subtle: this method shadows the method (activity).GetObject of likeActivity.activity.
+func (f *likeActivity) GetObject() Activity[Note] {
+	return Activity[Note]{
+		ID:     f.raw.Object.ID,
+		Type:   f.raw.Object.Type,
+		Actor:  &actor{object{f.raw.Object.ActivityActor}},
+		Object: &note{object{f.raw.Object.ActivityObject}},
+	}
+}
+
+// SetObject implements LikeActivitier.
+// Subtle: this method shadows the method (activity).SetObject of likeActivity.activity.
+func (f *likeActivity) SetObject(activity Activity[Note]) {
+	f.raw = &domain.ObjectOrLink{
+		Object: &domain.Object{
+			ID:             activity.ID,
+			Type:           activity.Type,
+			ActivityActor:  activity.Actor.GetRaw(),
+			ActivityObject: activity.Object.GetRaw(),
+		},
+	}
+}
