e17a0285ed02fd64c785923d8f02ec8e2fc0eb9c
Jan Tacakiewicz <jan.tacakiewicz@protonmail.com>
2026-01-10 22:29:07 +0100

    internal: processing: added FollowStatus util func

--- internal/processing/follow.go
+++ internal/processing/follow.go
@@ -2,6 +2,7 @@ package processing
 
 import (
 	"context"
+	"errors"
 
 	"github.com/kibirisu/borg/internal/ap"
 	"github.com/kibirisu/borg/internal/db"
@@ -34,4 +35,33 @@ func (p *processor) AcceptFollow(ctx context.Context, activity ap.FollowActiviti
 		Object: activity,
 	})
 	return p.client.Post(ctx, remoteAccount.InboxUri, accept.GetRaw().Object)
+}
+
+func (p *processor) FollowStatus(
+	ctx context.Context,
+	activity ap.FollowActivitier,
+) (db.Follow, error) {
+	uri := activity.GetURI()
+	if uri == "" {
+		return db.Follow{}, errors.New("invalid object")
+	}
+	follow, err := p.store.Follows().GetByURI(ctx, uri)
+	if err != nil {
+		activityData := activity.GetObject()
+		followerAccount, err := p.LookupActor(ctx, activityData.Actor)
+		if err != nil {
+			return follow, err
+		}
+		followedActor, err := p.LookupActor(ctx, activityData.Object)
+		if err != nil {
+			return follow, err
+		}
+		DBfollow, err := p.store.Follows().Create(ctx, db.CreateFollowParams{
+			Uri: "/users/" + followedActor.Username + "/followers/" + followerAccount.Username, //TODO
+			AccountID: followerAccount.ID,
+			TargetAccountID:  followedActor.ID,
+		})
+		return *DBfollow, err
+	}
+	return follow, nil
 }
