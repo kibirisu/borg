a7bd49ccf61ff0be5791425a187cf54069cc5880
Sofiia <sonyprh13.05@gmail.com>
2026-01-11 21:06:49 +0100

    add warning

--- web/src/components/pages/ExplorePage.tsx
+++ web/src/components/pages/ExplorePage.tsx
@@ -223,7 +223,34 @@ export default function ExplorePage() {
             </button>
           </form>
           {searchError && (
-            <p className="text-center text-sm text-red-600">{searchError}</p>
+            <div
+              className="flex items-start sm:items-center p-4 mb-4 text-sm text-fg-warning rounded-base bg-warning-soft text-yellow-800 bg-yellow-50 border border-yellow-200 rounded-lg"
+              role="alert"
+            >
+              <svg
+                className="w-4 h-4 me-2 shrink-0 mt-0.5 sm:mt-0"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+              <p>
+                <span className="font-medium me-1">Warning!</span>
+                {searchError === "Error during fetching client."
+                  ? "Sorry, we coudn't find this profile. Are you sure you entered username and instance right?"
+                  : searchError}
+              </p>
+            </div>
           )}
           {searchResult && (
             <div className="max-w-4xl mx-auto">
