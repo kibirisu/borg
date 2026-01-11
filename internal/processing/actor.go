038aab2b8cadccd4a3f572539e84f8fdb7a12276
Jan Tacakiewicz <jan.tacakiewicz@protonmail.com>
2026-01-11 19:50:06 +0100

    internal: processing: added a function that will recursively resolve all followers

--- internal/processing/actor.go
+++ internal/processing/actor.go
@@ -31,6 +31,56 @@ func (p *processor) FetchActorCollectionPage(ctx context.Context, collectionUri 
 	fetchedCollectionPage := ap.NewActorCollectionPage(colP)
 	return fetchedCollectionPage, nil
 }
+func (p *processor) LookupFollowers(ctx context.Context, account ap.Actorer) ([]db.Account, []db.Follow, error) {
+	type ActorOrigin struct {
+		Actor		ap.Actorer
+		FollowerOf	int32
+	}
+	accounts := make([]db.Account, 0)
+	follows := make([]db.Follow, 0)
+	toProcess := make([]ActorOrigin, 0)
+	toProcess = append(toProcess, ActorOrigin{account, -1})
+	for len(toProcess) > 0 {
+		cur := pop(&toProcess)
+		actorURI := cur.Actor.GetURI()
+		// skip if already in db (so theoretically already resolved)
+		_, err := p.store.Accounts().GetByURI(ctx, actorURI)
+		if err == nil {
+			continue
+		}
+		// fetch actor that hasnt yet been processed
+		acc, err := p.LookupActor(ctx, cur.Actor)
+		if err != nil {
+			continue
+		}
+		accounts = append(accounts, acc)
+		// if it wasnt root account (first one searched) then add the follow to parent
+		if cur.FollowerOf != -1 {
+			param := db.CreateFollowParams {
+				AccountID:			acc.ID,
+				TargetAccountID:	cur.FollowerOf, 
+			}
+			follow, err := p.store.Follows().Create(ctx, param)
+			if err == nil {
+				follows = append(follows, *follow)
+			}
+		}
+		// add this account followers as well
+		followersUri := cur.Actor.GetObject().Followers
+		page, err := p.FetchActorCollectionPage(ctx, followersUri)
+		if err != nil {
+			continue
+		}
+		for _, follower := range page.GetObject().Items {
+			toProcess = append(toProcess, ActorOrigin{
+				Actor:      follower, 
+				FollowerOf: acc.ID,
+			})
+		}
+	}
+
+	return accounts, follows, nil
+}
 
 func (p *processor) LookupActor(ctx context.Context, object ap.Actorer) (db.Account, error) {
 	uri := object.GetURI()
