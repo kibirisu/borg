361b3e521e6190d57c08cffb536fc1c9c925885d
Jan Tacakiewicz <jan.tacakiewicz@protonmail.com>
2026-01-11 21:12:00 +0100

    dual make dev setup

--- cmd/borg/borg.go
+++ cmd/borg/borg.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/kibirisu/borg/internal/config"
 	"github.com/kibirisu/borg/internal/server"
@@ -10,6 +11,12 @@ import (
 func main() {
 	ctx := context.Background()
 	conf := config.GetConfig()
+	fmt.Println("--- Config Test ---")
+    fmt.Printf("Env:  %s\n", conf.AppEnv)
+    fmt.Printf("Host: %s\n", conf.ListenHost)
+    fmt.Printf("Port: %s\n", conf.ListenPort)
+    fmt.Printf("DB:   %s\n", conf.DatabaseURL)
+    fmt.Println("-------------------")
 	s := server.New(ctx, conf)
 	panic(s.ListenAndServe())
 }
