bed269874fb1e80eded9b7549e65fc54557fb99b
kibirisu <szymon@wabisabi.cafe>
2026-01-18 14:22:04 +0100

    add undo follow ap type

--- internal/ap/follow.go
+++ internal/ap/follow.go
@@ -14,7 +14,14 @@ type followActivity struct {
 	activity
 }
 
-var _ FollowActivitier = (*followActivity)(nil)
+type undoFollowActivity struct {
+	activity
+}
+
+var (
+	_ FollowActivitier    = (*followActivity)(nil)
+	_ UndoFollowActiviter = (*undoFollowActivity)(nil)
+)
 
 func NewFollowActivity(from *domain.ObjectOrLink) FollowActivitier {
 	return &followActivity{activity{object{from}}}
@@ -60,4 +67,44 @@ func (f *followActivity) WithLink(link string) Objecter[Activity[Actor]] {
 func (f *followActivity) WithObject(activity Activity[Actor]) Objecter[Activity[Actor]] {
 	f.SetObject(activity)
 	return f
+}
+
+// GetObject implements UndoFollowActiviter.
+// Subtle: this method shadows the method (activity).GetObject of undoFollowActivity.activity.
+func (u *undoFollowActivity) GetObject() Activity[Activity[Actor]] {
+	return Activity[Activity[Actor]]{
+		ID:     u.raw.Object.ID,
+		Type:   u.raw.Object.Type,
+		Actor:  &actor{object{u.raw.Object.ActivityActor}},
+		Object: &followActivity{activity{object{u.raw.Object.ActivityObject}}},
+	}
+}
+
+// SetObject implements UndoFollowActiviter.
+// Subtle: this method shadows the method (activity).SetObject of undoFollowActivity.activity.
+func (u *undoFollowActivity) SetObject(activity Activity[Activity[Actor]]) {
+	u.raw = &domain.ObjectOrLink{
+		Object: &domain.Object{
+			ID:             activity.ID,
+			Type:           activity.Type,
+			ActivityActor:  activity.Actor.GetRaw(),
+			ActivityObject: activity.Object.GetRaw(),
+		},
+	}
+}
+
+// WithLink implements UndoFollowActiviter.
+// Subtle: this method shadows the method (activity).WithLink of undoFollowActivity.activity.
+func (u *undoFollowActivity) WithLink(link string) Objecter[Activity[Activity[Actor]]] {
+	u.SetLink(link)
+	return u
+}
+
+// WithObject implements UndoFollowActiviter.
+// Subtle: this method shadows the method (activity).WithObject of undoFollowActivity.activity.
+func (u *undoFollowActivity) WithObject(
+	activity Activity[Activity[Actor]],
+) Objecter[Activity[Activity[Actor]]] {
+	u.SetObject(activity)
+	return u
 }
